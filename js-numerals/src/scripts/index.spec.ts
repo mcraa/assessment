1b668826a348653961a6af75aea42925be08f489
mcraa <makra.peter@gmail.com>
2020-11-08 15:09:46 +0100

    test(init): mocha unit test setup with dummy test

---
+++ js-numerals/src/scripts/index.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import 'mocha';
+import { JSDOM } from 'jsdom';
+
+
+declare var global: any;
+const window = new JSDOM("").window;
+const document = window.document;
+global.window = window;
+global.document = document;
+
+import { App } from '../scripts/index'
+
+describe('App', () => {  
+
+  it('should get a number', () => {
+    const app = new App();
+
+    app.handleNumberParsing("none");
+
+    expect(app).to.be.not.null;
+  });
+})

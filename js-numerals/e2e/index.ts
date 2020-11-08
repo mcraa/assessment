eb5b9e118d9a4a66afc897d45c492879f19c5f88
mcraa <makra.peter@gmail.com>
2020-11-08 20:16:05 +0100

    test(e2e): added extra cases

--- js-numerals/e2e/index.ts
+++ js-numerals/e2e/index.ts
@@ -15,11 +15,18 @@ describe('Numbers converter', () => {
         { num: 2001, text: "two thousand and one" },
         { num: 1999, text: "nineteen hundred and ninety-nine" },
         { num: 17999, text: "seventeen thousand nine hundred and ninety-nine" }
+    ];
+
+    const others = [
+        { num: 1000000, text: "one million" },
+        { num: 154011, text: "one hundred and fifty-four thousand and eleven" },
+        { num: 1010, text: "one thousand and ten" },
+        { num: 1000, text: "one thousand" }
     ]
 
-    acceptance.forEach(ac => {        
+    acceptance.concat(others).forEach(ac => {        
         
-        it(`should display the correct number for ${ac.num}`, async () => {
+        it(`should display the correct text for ${ac.num}`, async () => {
             await browser.url('/');
             
             let input = await browser.$('#inputtext');

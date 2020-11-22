1111d43cd188d3c0c2ec2a9018e243affe4ec09a
mcraa <makra.peter@gmail.com>
2020-11-22 12:16:22 +0100

    test(app): added tests to refactored mounts

---
+++ node-todo/tests/App.ts
@@ -0,0 +1,32 @@
+import * as sinon from 'sinon'
+import { expect } from 'chai'
+import { App } from '../src/App'
+var express = require('express')
+
+describe('App', () => {
+    const sandbox = sinon.createSandbox()
+    afterEach(() => {
+        sandbox.restore();
+    })
+
+    it('should register routes', () => {
+        let expr = express()
+        let spy = sandbox.spy(expr, "use");
+        let router = express.Router();
+
+        let sut = new App(expr, "1212");
+        sut.mountRoutes("path", router);
+
+        expect(spy.calledOnceWithExactly("path", router)).to.be.true
+    })
+
+    it('should start server', () => {
+        let expr = express()
+        let spy = sandbox.spy(expr, "listen");
+
+        let sut = new App(expr, "1212");
+        sut.start()
+
+        expect(spy.calledOnce).to.be.true
+    })
+})
